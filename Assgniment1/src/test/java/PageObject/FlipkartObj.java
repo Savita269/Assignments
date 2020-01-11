74e32a87a67dbd9d9334d09e688be6368adbe691
Savita269 <58713170+Savita269@users.noreply.github.com>
2020-01-11 18:48:49 +0530

    First Assignment

---
+++ Assgniment1/src/test/java/PageObject/FlipkartObj.java
@@ -0,0 +1,24 @@
+package PageObject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class FlipkartObj {
+
+	public FlipkartObj(WebDriver driver)
+	{
+		PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(xpath = "//input[@type='text']")
+	public WebElement obj_flp_search;
+
+	@FindBy(xpath = "//svg[@viewBox='0 0 17 18']")
+	public WebElement obj_flp_clk_search;
+
+	@FindBy(xpath = "//div[@class='_1vC4OE _2rQ-NK']")
+	public WebElement obj_flp_price;
+	
+}
